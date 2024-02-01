dc2e1aa4be30b38a64387a43e62ae9b7073cf462
Duy Tran <67320223+duytran1511@users.noreply.github.com>
2024-02-01 09:28:37 +0700

    init

---
+++ Bai5_FSM_BaiTap/Bai5_FSM_PasswordDoor/Core/Inc/picture.h
@@ -0,0 +1,55 @@
+/*
+ * picture.h
+ *
+ *  Created on: Sep 13, 2023
+ *      Author: phamv
+ */
+
+#ifndef INC_PICTURE_H_
+#define INC_PICTURE_H_
+
+extern const unsigned char gImage_bk[19400];
+extern const unsigned char gImage_traffic[134400];
+
+/*
+ * img.c
+ *
+ *  Created on: Sep 1, 2023
+ *      Author: MyLinh
+ */
+
+extern const unsigned char gImage_bg[153600];
+
+extern const unsigned char gImage_protect[1920];
+
+extern const unsigned char gImage_wrong[11536];
+
+extern const unsigned char gImage_PW[12360];
+
+extern const unsigned char gImage_press1[20600];
+
+extern const unsigned char gImage_enterPW[19776];
+
+extern const unsigned char gImage_enterNewPW[19364];
+
+extern const unsigned char gImage_doorOpened[9476];
+
+extern const unsigned char gImage_wrongPW[21836];
+
+extern const unsigned char gImage_PWSuccessful[20600];
+
+extern const unsigned char gImage_door1[20020];
+
+extern const unsigned char gImage_door2[20020];
+
+extern const unsigned char gImage_door3[20020];
+
+extern const unsigned char gImage_door4[21600];
+
+extern const unsigned char gImage_door5[24840];
+
+extern const unsigned char gImage_door6[27048];
+
+extern const unsigned char gImage_door7[30912];
+
+#endif /* INC_PICTURE_H_ */
